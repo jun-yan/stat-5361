07822324d2db15729fce3e7340c5f39b462fbbf4
Jun Yan <jyan.stat@gmail.com>
2018-08-29 06:50:47 -0400

    install all needed packages in a batch

---
+++ stat5361-book/rpacks-pre.R
@@ -0,0 +1,13 @@
+## grep '^library' *.Rmd > rpacks.txt
+## sed -e 's|^[^(]*(||; s|).*$||' adv-r-rpacks.txt > adv-r.rpk
+
+rpacks <- unique(readLines("adv-r.rpk"))
+
+for (i in rpacks) {
+    if (!require(i)) inst <- try(install.packages(i))
+    if (inherits(inst, "try-error")) {
+        inst_git <- try(devtools::install_github(paste0("hadley/", i)))
+        if (inherits(inst_git, "try-error")) cat(i, "\n")
+    }
+}
+                                 
