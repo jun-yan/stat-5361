f92e426f90ebac1877c0abee0583220d50a6497a
Jun Yan <jyan.stat@gmail.com>
2018-08-30 07:10:25 -0400

    more missing packages

--- stat5361-book/rpacks-pre.R
+++ stat5361-book/rpacks-pre.R
@@ -3,6 +3,12 @@
 
 rpacks <- unique(readLines("rpack.lst"))
 
+## special from github
+devtools::install_github("r-lib/lobstr")
+devtools::install_github("r-lib/rlang")
+devtools::install_github("hadley/emo")
+install.packages("RSQLite")
+
 for (i in rpacks) {
     if (!require(i, character.only = TRUE)) {
         install.packages(i)
